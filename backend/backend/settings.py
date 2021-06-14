04b2b7545cbebd543a6496ed006d198ec9fda18d
kenedaf <faizakihel2@gmail.com>
2021-06-14 11:54:53 +0100

    First commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,13 +111,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'OPTIONS': {
+           'read_default_file': '/etc/mysql/my.cnf',
+       },
+   }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
